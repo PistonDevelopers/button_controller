dd823212f1f84efc4afe8f9ce9eab4bc788791bd
Sven Nilsen <bvssvni@gmail.com>
2019-06-09 16:27:08 +0200

    Published 0.11.0
    - Use sdl2_window for easier upgrading

--- examples/button_test.rs
+++ examples/button_test.rs
@@ -1,18 +1,28 @@
-extern crate piston_window;
+extern crate piston;
+extern crate graphics;
+extern crate sdl2_window;
+extern crate opengl_graphics;
 extern crate button_controller;
 
-use piston_window::*;
+use piston::*;
+use opengl_graphics::*;
+use graphics::*;
+use sdl2_window::Sdl2Window;
 use button_controller::{ButtonController, ButtonEvent, ButtonState};
 
 fn main() {
-    let mut window: PistonWindow = WindowSettings::new("button", [300; 2])
+    let opengl = OpenGL::V3_2;
+    let mut window: Sdl2Window = WindowSettings::new("button", [300; 2])
         .exit_on_esc(true)
+        .graphics_api(opengl)
         .build()
         .unwrap();
 
+    let mut events = Events::new(EventSettings::new());
+    let mut gl = GlGraphics::new(opengl);
     let mut click_me = ButtonController::new();
     let click_me_layout = [10.0, 60.0, 280.0, 180.0];
-    while let Some(e) = window.next() {
+    while let Some(e) = events.next(&mut window) {
         click_me.event(click_me_layout, math::identity(), &e);
         for e in &click_me.events {
             println!("{}", match *e {
@@ -24,15 +34,17 @@ fn main() {
             })
         }
 
-        window.draw_2d(&e, |c, g, _| {
-            clear([1.0; 4], g);
-            let color = match click_me.state(0.2) {
-                ButtonState::Inactive => [0.6, 0.6, 0.6, 1.0],
-                ButtonState::Hover => [0.4, 0.4, 0.4, 1.0],
-                ButtonState::Press => [0.1, 0.1, 0.1, 1.0],
-                ButtonState::Cancel => [0.3, 0.2, 0.2, 1.0],
-            };
-            rectangle(color, click_me_layout, c.transform, g);
-        });
+        if let Some(args) = e.render_args() {
+            gl.draw(args.viewport(), |c, g| {
+                clear([1.0; 4], g);
+                let color = match click_me.state(0.2) {
+                    ButtonState::Inactive => [0.6, 0.6, 0.6, 1.0],
+                    ButtonState::Hover => [0.4, 0.4, 0.4, 1.0],
+                    ButtonState::Press => [0.1, 0.1, 0.1, 1.0],
+                    ButtonState::Cancel => [0.3, 0.2, 0.2, 1.0],
+                };
+                rectangle(color, click_me_layout, c.transform, g);
+            });
+        }
     }
 }
